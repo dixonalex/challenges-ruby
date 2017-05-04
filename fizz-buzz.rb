729f6319c07971efb1bf8d74081045f1932da6b4
= <alexanderldixon@gmail.com>
2017-05-04 18:55:14 -0500

    fizz buzz challenge

---
+++ fizz-buzz.rb
@@ -0,0 +1,4 @@
+require 'fizz_buzz'
+
+fb = FizzBuzz.new(1,100)
+fb.print()
---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,35 @@
+# "Write a program that prints the numbers from 1 to 100. 
+# But for multiples of three print “Fizz” instead of the number 
+# and for the multiples of five print “Buzz”. 
+# For numbers which are multiples of both three and five print “FizzBuzz”."
+
+class FizzBuzz
+  def lowBound
+    1
+  end
+  def highBound
+    100
+  end
+  def initialize (lowBound, highBound)
+    @lowBound = lowBound
+    @highBound = highBound
+  end
+  def print(*args)
+    for i in @lowBound..@highBound
+      if i % fizzMod == 0 and i % buzzMod == 0 
+        puts "Value #{i}: FizzBuzz"
+      elsif i % fizzMod == 0 
+        puts "Value #{i}: Fizz"
+      elsif i % buzzMod == 0 
+        puts "Value #{i}: Buzz"
+      end
+    end
+  end
+  private
+  def fizzMod
+    3
+  end
+  def buzzMod
+    5
+  end
+end
