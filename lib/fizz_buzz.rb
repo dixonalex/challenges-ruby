5b257ada92e40ba0154ef87750a1bbd55a92e1a8
= <alexanderldixon@gmail.com>
2017-05-04 19:07:26 -0500

    I dont really need to initialize the properties

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -4,33 +4,20 @@
 # For numbers which are multiples of both three and five print “FizzBuzz”."
 
 class FizzBuzz
-  def lowBound
-    1
-  end
-  def highBound
-    100
-  end
   def initialize (lowBound, highBound)
     @lowBound = lowBound
     @highBound = highBound
   end
   def print(*args)
     for i in @lowBound..@highBound
-      if i % fizzMod == 0 and i % buzzMod == 0 
+      if i % 3 == 0 and i % 5 == 0 
         puts "FizzBuzz"
-      elsif i % fizzMod == 0 
+      elsif i % 3 == 0 
         puts "Fizz"
-      elsif i % buzzMod == 0 
+      elsif i % 5 == 0 
         puts "Buzz"
       else puts i
       end
     end
-  end
-  private
-  def fizzMod
-    3
-  end
-  def buzzMod
-    5
   end
 end
